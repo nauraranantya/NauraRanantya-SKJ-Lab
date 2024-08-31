884cc4053a459e629bac70eaa78fa1dd3b3ab37b
nauraranantya <nauraranantyakinanti@gmail.com>
2024-08-31 18:21:39 +0700

    Add files via upload

---
+++ Assignment1/Assembly to C++/multiply.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    // Define the variables
+    int num1 = 5;
+    int num2 = 10;
+    int result = 0;
+
+    // Perform multiplication
+    result = num1 * num2;
+
+    // Output the result
+    std::cout << "The result of multiplication is: " << result << std::endl;
+
+    // Exit the program
+    return 0;
+}
